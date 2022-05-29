d4e0fb34d00c2feadb526e8946162a448ee37a60
Pavel Luianchenko <pavel.lukianchenko@hotmail.com>
2022-05-29 14:21:23 +0300

    first commint

---
+++ script.js
@@ -0,0 +1,22 @@
+S = Math.PI;
+R = 10;
+H = 20;
+V = S * Math.pow(R, 2) * H;
+// V = S * (R * R) * H;
+
+console.log("**************");
+console.log("Обьем цилиндра с площадью основы S =", S, "радиусом R =", R, "и высотой H =", H, "равен:");
+console.log("--------------------");
+console.log("V =", V);
+console.log("-------------------");
+console.log("end.");
+
+// star = "**************";
+// dash = "--------------------";
+
+// console.log(star);
+// console.log("Обьем цилиндра с площадью основы S =", S, "радиусом R =", R, "и высотой H =", H, "равен:");
+// console.log(dash);
+// console.log("V =", S * Math.pow(R, 2) * H);
+// console.log(dash);
+// console.log("end.");
